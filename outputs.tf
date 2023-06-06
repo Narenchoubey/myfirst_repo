3c087e8e1564de14851fa470a4d1228c79bb65a0
Narenchoubey <135047824+Narenchoubey@users.noreply.github.com>
2023-06-06 22:23:44 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "instance_ip" {
-  value = google_compute_instance.default[count.index].network_interface[0].access_config[0].nat_ip
+  value = null_resource.instance_ips[*].triggers.instance_index
 }
