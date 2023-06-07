27d2514d7533aa10da92952e8a7edebbe231bafe
Narenchoubey <135047824+Narenchoubey@users.noreply.github.com>
2023-06-07 13:27:21 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,6 @@
 output "instance_ip" {
   value = null_resource.instance_ips[*].triggers.instance_index
 }
+output "instance_ip2" {
+  value = "${google_compute_address.static-ip-address.address}"
+}
